1dad7f54c78ee6647d0a4ece2bb3efd5067e8763
laurenedb7 <73172295+laurenedb7@users.noreply.github.com>
2022-05-10 12:22:16 +0200

    changement form inscription et ajout annonce

--- front/src/pages/Annonces.vue
+++ front/src/pages/Annonces.vue
@@ -22,29 +22,33 @@
                 @reset="onReset"
                 class="q-gutter-md">
 
+                <q-btn-toggle
+                  v-model="groupType"
+                  spread
+                  no-caps
+                  toggle-color="primary"
+                  color="white"
+                  text-color="black"
+                  :options="optionsType"
+                />
+
                 <q-input 
                   filled
                   v-model="title" 
-                  label="Title"
+                  label="Titre de l'annonce"
                   lazy-rules
-                  :rules="[ val => val && val.length > 0 || 'Please type something']">
+                  :rules="[ val => val && val.length > 0 || 'Champ obligatoire']">
                   <template v-slot:prepend>
                     <q-icon name="title" />
                   </template>
                 </q-input>
 
-                <q-option-group
-                  v-model="groupType"
-                  :options="optionsType"
-                  color="primary"
-                />
-
                 <q-input 
                   filled
                   v-model="place" 
-                  label="Place"
+                  label="Adresse"
                   lazy-rules
-                  :rules="[ val => val && val.length > 0 || 'Please type something']">
+                  :rules="[ val => val && val.length > 0 || 'Champ obligatoire']">
                   <template v-slot:prepend>
                     <q-icon name="place" />
                   </template>
@@ -54,18 +58,18 @@
                   v-model="description"
                   filled
                   type="textarea"
-                  label="Describe your accomodation"
+                  label="Décriver votre logement"
                 />
 
                 <q-input
                   filled
                   type="number"
                   v-model="numberOfPeople"
-                  label="Number max of people"
+                  label="Nombre maximum de personnes"
                   lazy-rules
                   :rules="[
-                    val => val !== null && val !== '' || 'Please a number max of people',
-                    val => val > 0 && val < 30 || 'Please type a real number'
+                    val => val !== null && val !== '' || 'Champ obligatoire',
+                    val => val > 0 && val < 30 || 'Nombre choisi incorrect'
                   ]"
                 />
 
@@ -73,11 +77,11 @@
                   filled
                   type="number"
                   v-model="numberOfBedrooms"
-                  label="Number of bedrooms"
+                  label="Nombre de chambres"
                   lazy-rules
                   :rules="[
-                    val => val !== null && val !== '' || 'Please a number of bedrooms',
-                    val => val > 0 && val < 30 || 'Please type a real number'
+                    val => val !== null && val !== '' || 'Champ obligatoire',
+                    val => val > 0 && val < 30 || 'Nombre choisi incorrect'
                   ]"
                 />
 
@@ -85,11 +89,23 @@
                   filled
                   type="number"
                   v-model="numberOfBeds"
-                  label="Number of beds"
+                  label="Nombre de lits simples"
                   lazy-rules
                   :rules="[
-                    val => val !== null && val !== '' || 'Please a number of beds',
-                    val => val > 0 && val < 30 || 'Please type a real number'
+                    val => val !== null && val !== '' || 'Champ obligatoire',
+                    val => val > 0 && val < 30 || 'Nombre choisi incorrect'
+                  ]"
+                />
+
+                <q-input
+                  filled
+                  type="number"
+                  v-model="numberOfDoubleBeds"
+                  label="Nombre de lits doubles"
+                  lazy-rules
+                  :rules="[
+                    val => val !== null && val !== '' || 'Champ obligatoire',
+                    val => val > 0 && val < 30 || 'Nombre choisi incorrect'
                   ]"
                 />
 
@@ -97,22 +113,25 @@
                   filled
                   type="number"
                   v-model="numberOfBathrooms"
-                  label="Number of bathrooms"
+                  label="Nombre de salles de bain"
                   lazy-rules
                   :rules="[
-                    val => val !== null && val !== '' || 'Please a number of bathrooms',
-                    val => val > 0 && val < 30 || 'Please type a real number'
+                    val => val !== null && val !== '' || 'Champ obligatoire',
+                    val => val > 0 && val < 30 || 'Nombre choisi incorrect'
                   ]"
                 />
 
-                <q-option-group
-                  v-model="group"
-                  :options="options"
-                  color="green"
-                  type="checkbox"
-                />
+                <div class="bg-grey-2 q-pa-sm rounded-borders">
+                  <p class="text-weight-medium">Equipements présents :</p>
+                  <q-option-group
+                    v-model="group"
+                    :options="options"
+                    color="green"
+                    type="checkbox"
+                  />
+                </div>
 
-                <q-btn flat color="bg-secondary" icon="event" label="Chose dates" @click="displayDates = true" />
+                <q-btn flat color="bg-secondary" icon="event" label="Choisissez les disponibilités" @click="displayDates = true" />
                 <q-dialog v-model="displayDates">
                   <q-date v-model="days" range multiple />
                 </q-dialog>
@@ -122,17 +141,17 @@
                   filled 
                   multiple
                   v-model="model" 
-                  label="Pictures">
+                  label="Photos">
                   <template v-slot:prepend>
                     <q-icon name="cloud_upload" />
                   </template>
                 </q-file>
               
 
-                <q-toggle v-model="accept" label="I accept the license and terms" />
+                <q-toggle v-model="accept" label="J'ai lu et j'accepte les conditions d'utilisation." />
 
                 <div>
-                  <q-btn label="Publish" type="submit" color="primary"/>
+                  <q-btn label="Publier l'annonce" type="submit" color="primary"/>
                   <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
                 </div>
               </q-form>
@@ -159,6 +178,7 @@ export default {
     const numberOfPeople = ref(null)
     const numberOfBedrooms = ref(null)
     const numberOfBeds = ref(null)
+    const numberOfDoubleBeds = ref(null)
     const numberOfBathrooms = ref(null)
     const accept = ref(false)
 
@@ -172,6 +192,7 @@ export default {
       numberOfPeople,
       numberOfBedrooms,
       numberOfBeds,
+      numberOfDoubleBeds,
       numberOfBathrooms,
       accept,
 
@@ -179,15 +200,15 @@ export default {
 
       optionsType: [
         {
-          label: 'House',
+          label: 'Maison',
           value: 'op1'
         },
         {
-          label: 'Apartment',
+          label: 'Appartement',
           value: 'op2'
         },
         {
-          label: 'Other',
+          label: 'Autre',
           value: 'op3'
         }
       ],
@@ -196,11 +217,11 @@ export default {
 
       options: [
         {
-          label: 'Garden',
+          label: 'Jardin',
           value: 'op1'
         },
         {
-          label: 'Swimming pool',
+          label: 'Piscine',
           value: 'op2'
         },
         {
@@ -208,7 +229,7 @@ export default {
           value: 'op3'
         },
         {
-          label: 'Kitchen with equipments',
+          label: 'Cuisine équipée',
           value: 'op4'
         },
         {
@@ -216,8 +237,24 @@ export default {
           value: 'op5'
         },
         {
-          label: 'Smoke detector',
+          label: 'Canapé convertible',
           value: 'op6'
+        },
+        {
+          label: 'Machine à laver',
+          value: 'op7'
+        },
+        {
+          label: 'Lave-vaisselle',
+          value: 'op8'
+        },
+        {
+          label: 'Sèche cheveux',
+          value: 'op9'
+        },
+        {
+          label: 'Détecteur de fumée',
+          value: 'op10'
         }
       ],
 
@@ -247,6 +284,7 @@ export default {
         numberOfPeople.value = null
         numberOfBedrooms.value = null
         numberOfBeds.value = null
+        numberOfDoubleBeds.value = null
         numberOfBathrooms.value = null
         accept.value = false
       }
--- front/src/pages/Inscription.vue
+++ front/src/pages/Inscription.vue
@@ -4,18 +4,30 @@
 
     <div class="row justify-evenly">
 
-    <q-form
-      @submit="subscribeOnSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
+    <div class="q-pa-md">
+    <q-stepper
+      v-model="step"
+      vertical
+      color="primary"
+      animated
     >
 
-      <q-input
+      <q-step
+        :name="1"
+        title="Informations personnelles"
+        icon="settings"
+        :done="step > 1"
+      >
+
+      <p>Veuillez saisir vos informations personnelles nécessaires à l'inscription <br/> sur la plateforme. 
+        <br/>Tous les champs sont obligatoires.</p>
+
+        <q-input
         filled
         v-model="Prénom"
         label="Prénom *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'Champ obligatoire']"
       />
 
       <q-input
@@ -23,7 +35,7 @@
         v-model="Nom"
         label="Nom *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'Champ obligatoire']"
       />
 
       <q-input
@@ -33,7 +45,7 @@
         mask="##/##/####"
         fill-mask
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'Champ obligatoire']"
       >
       <template v-slot:prepend>
           <q-icon name="event" />
@@ -45,7 +57,7 @@
         v-model="téléphone"
         label="Numéro de téléphone *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'Champ obligatoire']"
       >
       <template v-slot:prepend>
           <q-icon name="phone" />
@@ -57,21 +69,36 @@
         v-model="mail"
         label="Adresse mail *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something',]"
+        :rules="[ val => val && val.length > 0 || 'Champ obligatoire',]"
       >
       <template v-slot:prepend>
           <q-icon name="mail" />
         </template>
       </q-input>
 
-      <q-input 
+        <q-stepper-navigation>
+          <q-btn @click="step = 2" color="primary" label="Continuer" />
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="2"
+        title="Mot de passe"
+        icon="key"
+        :done="step > 2"
+      >
+        <p>Veuillez choisir un mot de passe. <br/>
+        Votre mot de passe doit contenir minimum 8 caractères et doit contenir <br/>
+          au moins une majuscule, une minuscule et un caractère spécial.</p>
+
+        <q-input 
         v-model="password" 
         filled :type="isPwd ? 'password' : 'text'" 
-        label="Password *"
+        label="Mot de passe *"
         lazy-rules
         :rules="[ 
-            val => val && val.length > 0 || 'Please type something',
-            val => val.length > 7 && val.length < 30 || 'Your password is not secure enough']"
+            val => val && val.length > 0 || 'Champ obligatoire',
+            val => val.length > 7 && val.length < 30 || 'Mot de passe non sécurisé']"
       >
         <template v-slot:append>
           <q-icon
@@ -85,11 +112,11 @@
       <q-input 
         v-model="confirmPassword" 
         filled :type="isConfirmPwd ? 'password' : 'text'" 
-        label="Confirm Password *"
+        label="Confirmation mot de passe *"
         lazy-rules
         :rules="[ 
-              val => val && val.length > 0 || 'Please type something',
-              val => val == password || 'Wrong password confirmation']"
+              val => val && val.length > 0 || 'Champ obligatoire',
+              val => val == password || 'Confirmation de mot de passe incorrecte']"
       >
         <template v-slot:append>
           <q-icon
@@ -100,13 +127,77 @@
         </template>
       </q-input>
 
-      <q-toggle v-model="accept" label="J'ai lu et j'accepte les conditions d'utilisation." />
+        <q-stepper-navigation>
+          <q-btn @click="step = 3" color="primary" label="Continuer" />
+          <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
 
-      <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
+      <q-step
+        :name="3"
+        title="Photo de profil"
+        icon="photo"
+      >
+
+      <p>Veuillez choisir une photo de profil. <br/>
+      Votre photo ne doit pas dépasser les 5Mo. Vérifier la taille de votre photo <br/> avant importation.</p>
+        <q-file
+          v-model="file"
+          label="Importer"
+          filled
+          style="max-width: 500px"
+        >
+        <template v-slot:prepend>
+          <q-icon name="attach_file" />
+        </template>
+      </q-file>
+
+        <q-stepper-navigation>
+          <q-btn @click="step= 4" color="primary" label="Continuer" />
+          <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+
+      <q-step
+        :name="4"
+        title="Conditions générales d'utilisation"
+        icon="format_size"
+      >
+
+      <p>Veuillez lire et accepter les conditions générales d'utilisation.</p>
+
+      <q-btn label="Conditions générales d'utilisation" color="secondary" @click="fixed = true" /><br/>
+
+      <q-dialog v-model="fixed">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Conditions générales d'utilisation : </div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-section style="max-height: 50vh" class="scroll">
+            <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions align="right">
+            <q-btn flat label="Accept" color="primary" v-close-popup @click="accept = true"/>
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+      <br/>
+       <q-toggle v-model="accept" disable label="J'ai lu et j'accepte les conditions d'utilisation." />
+        
+
+        <q-stepper-navigation>
+          <q-btn @click="subscribeOnSubmit" color="primary" label="Envoyer" />
+          <q-btn flat @click="step = 3" color="primary" label="Back" class="q-ml-sm" />
+        </q-stepper-navigation>
+      </q-step>
+    </q-stepper>
+  </div>
 
   </div>
   </q-page>
@@ -134,6 +225,7 @@ export default {
     return {
       isPwd: ref(true),
       isConfirmPwd: ref(true),
+      step: ref(1),
 
       Prénom,
       Nom,
@@ -142,6 +234,8 @@ export default {
       mail,
       password,
       confirmPassword,
+      file: ref(null),
+      fixed: ref(false),
       accept,
 
       subscribeOnSubmit () {
@@ -150,7 +244,7 @@ export default {
             color: 'red-5',
             textColor: 'white',
             icon: 'warning',
-            message: 'You need to accept the license and terms first'
+            message: 'Vous devez acceptez les conditions d\'utilisations'
           })
         }
         else {
