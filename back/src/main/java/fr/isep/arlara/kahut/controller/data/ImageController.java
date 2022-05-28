0c9a3a66a2e820c79856b58afe82e12b245bec43
Icodak <24356594+Icodak@users.noreply.github.com>
2022-05-28 10:39:54 +0200

    refactor image controller & service

--- back/src/main/java/fr/isep/arlara/kahut/controller/data/ImageController.java
+++ back/src/main/java/fr/isep/arlara/kahut/controller/data/ImageController.java
@@ -1,17 +1,14 @@
 package fr.isep.arlara.kahut.controller.data;
 
-import fr.isep.arlara.kahut.model.database.Image;
-import fr.isep.arlara.kahut.repository.ImageRepository;
+import fr.isep.arlara.kahut.service.data.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController
@@ -20,26 +17,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ImageController {
 
-    @Autowired
-    ImageRepository imageRepository;
+    private final ImageService imageService;
 
     @PostMapping("/api/image")
-    UUID uploadImage(@RequestParam MultipartFile multipartImage) throws Exception {
-        Image image = new Image();
-        image.setLegend(multipartImage.getName());
-
-        image.setData(multipartImage.getBytes());
-
-        return imageRepository.save(image).getId();
+    ResponseEntity<UUID> uploadImage(@RequestParam MultipartFile multipartImage) throws IOException {
+        return imageService.uploadImage(multipartImage);
     }
 
     @GetMapping(value = "/api/resources/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
     Resource downloadImage(@PathVariable UUID imageId) {
-        byte[] image = imageRepository.findById(imageId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
-                .getData();
-
-        return new ByteArrayResource(image);
+        return imageService.downloadImage(imageId);
     }
 
 }
