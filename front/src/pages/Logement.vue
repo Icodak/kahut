012a379101af45beeedcf11a02890405e682b77d
Landry Mamo <49226509+LNM21@users.noreply.github.com>
2022-05-29 19:00:16 +0200

    Connexions back-front Page Logement
    La page charge les infos nécessaires (tags exceptés). Reste les commentaires et les tags

--- front/src/pages/Logement.vue
+++ front/src/pages/Logement.vue
@@ -1,40 +1,108 @@
 <template>
   <q-page>
-    <h3 class="row justify-evenly">
-      Appartement 3 pièces en plein coeur de Paris
-    </h3>
+    <div class="row" style="margin: 1%">
+      <div class="col-4">
+        <q-img
+          style="max-width: 300px"
+          :ratio="4 / 3"
+          src="housing.images"
+        />
 
-    <q-img
-      style="max-width: 300px"
-      :ratio="4 / 3"
-      src="https://img.freepik.com/photos-gratuite/conception-appartement-studio-moderne-chambre-espace-vie_1262-12375.jpg?t=st=1652184716~exp=1652185316~hmac=925ec8f9ac803a9cd96d7b11b87e144b6b9b7c94ae363829992ab11b2fd88b5f&w=1800"
-    />
+        <q-card-section>
+          <div class="row no-wrap items-center col-8">
+            <div class="text-h6 ellipsis">
+              {{ housing.title }}
+            </div>
+          </div>
+          {{ housing.location }} <br />
+          <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+          <span class="text-caption text-grey q-ml-sm">{{housing.stars}}</span>
+        </q-card-section>
 
-    <div class="row no-wrap items-center">
-      <div class="col text-h6 ellipsis">
-        Appartement 3 pièces en plein coeur de Paris
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">5 {{ numberOfPeople }} voyageurs</div>
+          <div class="text-caption text-grey">
+            2 {{ numberOfBedrooms }} chambres . 1 {{ numberOfBeds }} lit simple
+            . 1 {{ numberOfDoubleBeds }} lit double <br />
+            Wifi . Chauffage . Télévision
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">Equipements</div>
+          <div class="text-caption text-grey">
+            Canapé convertible . Machine à laver
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">Requêtes</div>
+          <div class="text-caption text-grey">
+            Arroser les plantes . Non-Fumeur . Chiens autorisés
+          </div>
+        </q-card-section>
+      </div>
+      <div class="col-8" style="margin-top:10%; margin left:5%;">
+        <h6>Description</h6>
+        <p>
+          {{ housing.description }}
+        </p>
+
+<div style="max-width: 350px">
+    <q-list bordered>
+      <q-title>
+        <h6 class="q-ma-md text-center"></h6>
+      </q-title>
+      <q-item>
+        <q-item-section style="font-weight: bold;">{{housing.author.description}}</q-item-section>
+      </q-item>
+      <q-item>
+        <q-item-section avatar>
+          <q-icon color="primary" name="mail" />
+        </q-item-section>
+
+        <q-item-section>{{housing.author.email}}</q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section avatar>
+          <q-icon color="primary" name="phone" />
+        </q-item-section>
+
+        <q-item-section>{{housing.author.phone}}</q-item-section>
+      </q-item>
+    </q-list>
+
+  </div>
+  
+
+
       </div>
     </div>
-
-    <q-rating size="18px" v-model="stars" :max="5" color="primary" />
-    <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
-
-    <q-card-section class="q-pt-none">
-      <div class="text-subtitle1">5 voyageurs</div>
-      <div class="text-caption text-grey">
-        2 chambres . 1 lit double . 1 lit simple . 1 canapé convertible<br />
-        Wifi . Chauffage . Télévision
-      </div>
-    </q-card-section>
-
-    <q-separator />
-
-    <q-card-actions>
-      <q-btn icon="house"  label="Disponible" />
-      <q-btn color="primary" icon="edit" label="Modifier" />
-    </q-card-actions>
   </q-page>
 </template>
 
-<script>
+<script lang="ts">
+import { onMounted, ref } from "vue";
+import { api } from "src/boot/axios";
+
+export default {
+  setup () {
+const housing = ref({});
+
+onMounted(async () => {
+  const response = await api.get(
+    "housing/467fd92c-5fe2-4a67-ac7e-15bddb9fb01e"
+  );
+  housing.value = response.data;
+})
+
+
+    
+console.log(housing)
+    return {
+      housing,
+    };
+  },
+};
 </script>
--- front/src/services/logement.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import LocalStorage from 'quasar'
-
-const api = axios.create({ baseURL: 'http://localhost:8080' });
-export async function getHousing(){
-    
-}
