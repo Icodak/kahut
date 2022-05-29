7beb7a50ab1b37171bcb90da37b9db0ae37d2af6
Landry Mamo <49226509+LNM21@users.noreply.github.com>
2022-05-29 22:46:58 +0200

    Update Logement.vue
    Correction bug affichage

--- front/src/pages/Logement.vue
+++ front/src/pages/Logement.vue
@@ -1,47 +1,6 @@
 <template>
   <q-page>
     <div class="row" style="margin: 1%">
-      <div class="col-4">
-        <q-img
-          style="max-width: 300px"
-          :ratio="4 / 3"
-          src="housing.images"
-        />
-
-        <q-card-section>
-          <div class="row no-wrap items-center col-8">
-            <div class="text-h6 ellipsis">
-              {{ housing.title }}
-            </div>
-          </div>
-          {{ housing.location }} <br />
-          <q-rating size="18px" v-model="stars" :max="5" color="primary" />
-          <span class="text-caption text-grey q-ml-sm">{{housing.stars}}</span>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">5 {{ numberOfPeople }} voyageurs</div>
-          <div class="text-caption text-grey">
-            2 {{ numberOfBedrooms }} chambres . 1 {{ numberOfBeds }} lit simple
-            . 1 {{ numberOfDoubleBeds }} lit double <br />
-            Wifi . Chauffage . Télévision
-          </div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">Equipements</div>
-          <div class="text-caption text-grey">
-            Canapé convertible . Machine à laver
-          </div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <div class="text-subtitle1">Requêtes</div>
-          <div class="text-caption text-grey">
-            Arroser les plantes . Non-Fumeur . Chiens autorisés
-          </div>
-        </q-card-section>
-      </div>
       <div class="col-8" style="margin-top:10%; margin left:5%;">
         <h6>Description</h6>
         <p>
@@ -51,17 +10,17 @@
 <div style="max-width: 350px">
     <q-list bordered>
       <q-title>
-        <h6 class="q-ma-md text-center"></h6>
+        <h6 class="q-ma-md text-center">{{owner.fullName}}</h6>
       </q-title>
       <q-item>
-        <q-item-section style="font-weight: bold;">{{housing.author.description}}</q-item-section>
+        <q-item-section style="font-weight: bold;">{{owner.description}}</q-item-section>
       </q-item>
       <q-item>
         <q-item-section avatar>
           <q-icon color="primary" name="mail" />
         </q-item-section>
 
-        <q-item-section>{{housing.author.email}}</q-item-section>
+        <q-item-section>{{owner.email}}</q-item-section>
       </q-item>
 
       <q-item>
@@ -69,7 +28,7 @@
           <q-icon color="primary" name="phone" />
         </q-item-section>
 
-        <q-item-section>{{housing.author.phone}}</q-item-section>
+        <q-item-section>{{owner.phone}}</q-item-section>
       </q-item>
     </q-list>
 
@@ -89,12 +48,20 @@ import { api } from "src/boot/axios";
 export default {
   setup () {
 const housing = ref({});
+const owner = ref({});
 
 onMounted(async () => {
   const response = await api.get(
-    "housing/467fd92c-5fe2-4a67-ac7e-15bddb9fb01e"
+    "housing/eff9417c-828b-48fd-b382-852fe3d1b38c"
   );
   housing.value = response.data;
+})
+
+onMounted(async () => {
+  const response2 = await api.get(
+    "housing/eff9417c-828b-48fd-b382-852fe3d1b38c"
+  );
+  owner.value = response2.data.author;
 })
 
 
@@ -102,6 +69,7 @@ onMounted(async () => {
 console.log(housing)
     return {
       housing,
+      owner,
     };
   },
 };
