f278c174d25d8805b12f0e6ff972b0aa0c53778a
Icodak <24356594+Icodak@users.noreply.github.com>
2022-05-30 05:35:18 +0200

    front search

--- front/src/pages/Recherche.vue
+++ front/src/pages/Recherche.vue
@@ -9,119 +9,191 @@
 <template>
   <q-page>
     <div class="q-pa-md">
-      <div class ="column" style ="height: 100px;">
-
-      <searchBarComponent> </searchBarComponent>
-    </div>
-    </div>
-
-
-
-    <div class="q-pa-md row items-start q-gutter-md">
-
-
-      <q-card class="my-card">
-        <div class="row" style="margin:1%">
-          <div class="col-4">
-
-
-
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">5 voyageurs</div>
-              <div class="text-caption text-grey">
-
-                {{housing}}
-
+      <div class="column" style="height: 100px">
+        <q-form ref="search" class="q-gutter-md" @submit="searchHousing()">
+          <div class="barre_de_recherche">
+            <div class="row">
+              <div class="col-2">
+                <q-input
+                  filled
+                  v-model="destination"
+                  label="Où aller ?"
+                  lazy-rules
+                  :rules="[
+                    (val) =>
+                      (val && val.length > 3) ||
+                      'Entrez une destination valide',
+                  ]"
+                >
+                  <template v-slot:prepend>
+                    <q-icon filled name="place" />
+                  </template>
+                </q-input>
               </div>
-            </q-card-section>
-          </div>
-          <div class="col-8" style="margin-top:10%; margin left:5%">
 
-          </div>
-        </div>
-        <q-separator />
-        <q-card-actions>
+              <div class="col-1,5">
+                <q-select
+                  filled
+                  v-model="multipleDevoirs"
+                  multiple
+                  :options="optionsDevoirs"
+                  label="A faire"
+                  style="width: 250px"
+                />
+              </div>
 
-        </q-card-actions>
-      </q-card>
+              <div class="col-1,5">
+                <q-select
+                  filled
+                  v-model="multipleLimitations"
+                  multiple
+                  :options="optionsLimitations"
+                  label="Limitations"
+                  style="width: 250px"
+                />
+              </div>
+
+              <div class="col-2">
+                <q-select
+                  filled
+                  v-model="numberTravellers"
+                  :options="optionsTravellers"
+                  label="Combien ?"
+                />
+              </div>
+
+              <div class="col-1">
+                <q-btn
+
+                  type="submit"
+                  icon="search"
+                  color="orange"
+                  size="xl"
+                />
+              </div>
+            </div>
+          </div>
+        </q-form>
+      </div>
     </div>
 
     <div class="q-pa-md row items-start q-gutter-md">
-      <q-card class="my-card" v-for="data in housing" :key="data">
-        <div class="row" style="margin:1%">
+      <q-card class="my-card" v-for="housing in housings" :key="housing">
+        <div class="row" style="margin: 1%">
           <div class="col-4">
             <q-img
               style="max-width: 300px"
               :ratio="4 / 3"
-              src="https://cdn.bioalaune.com/img/article/thumb/900x500/24466-10_maisons_magiques_au_coeur_de_la_nature_dans_lesquelles_vous_adoreriez_vivre_compressed.jpg"
+              :src=housing.imageId
             />
 
-            <q-card-section >
-              <div class="row no-wrap items-center col-3" >
-                <div class="text-h9 ellipsis" >
-{{data.title}}
-</div>
+            <q-card-section>
+              <div class="row no-wrap items-center col-3">
+                <div class="text-h9 ellipsis">
+                  {{ housing.title }}
+                </div>
               </div>
-
 
               <span class="text-caption text-grey q-ml-sm"></span>
             </q-card-section>
 
-
             <q-card-section class="q-pt-none">
               <div class="text-subtitle1"></div>
               <div class="text-caption text-grey">
-                {{data.description}}
+                {{ housing.description }}
               </div>
             </q-card-section>
           </div>
-          <div class="col-8 text-h4 ellipsis" style="margin-top:10%; margin left:5%">
-
-                La Kahute de <br>
-                <p class="text-h4">
-                  à {{data.location}}
-                  </p>
-
+          <div
+            class="col-8 text-h4 ellipsis"
+            style="margin-top:10%; margin left:5%"
+          >
+            La Kahute de <br />
+            <p class="text-h4">à {{ housing.location }}</p>
           </div>
         </div>
         <q-separator />
         <q-card-actions>
-          <q-btn color="primary" icon="House" label="Avoir plus d'informations" to="/logement" />
+          <q-btn
+            color="primary"
+            icon="House"
+            label="Avoir plus d'informations"
+            to="/logement"
+            v-on:click="storeHousingId(housing.id)"
+          />
         </q-card-actions>
       </q-card>
     </div>
   </q-page>
 </template>
 
-<script lang="ts">
-
-import * as Vue from 'vue' // in Vue 3
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import searchBarComponent from 'src/components/searchBar.vue';
+<script >
 import { onMounted, ref } from "vue";
-import {api} from"src/boot/axios";
-import destination from 'src/components/searchBar.vue';
-import searchBarVue from 'src/components/searchBar.vue';
-import { error } from 'console';
-export default{
+import { api } from "src/boot/axios";
+import { LocalStorage, SessionStorage } from "quasar";
 
-  components : {searchBarComponent},
-setup(){
-const housing = ref ({});
-onMounted(async() => {
-   const response = await api.post( "/query",
-{destination : "Paris"}
-)
-housing.value = response.data;
-})
-console.log(housing.value)
-    return {
-      housing
+export default {
+  setup() {
+    const optionsDevoirs = [
+      "S'occuper des animaux",
+      "S'occuper des plantes",
+      "Nettoyer la Maison",
+      "Entretenir le jardin",
+    ];
+    const optionsLimitations = [
+      "Interdiction de fumer",
+      "Interdit d'avoir des animaux",
+      "Les enfants de moins de 3 ans sont interdit",
+      "Pas de bruit après 20h",
+    ];
+    const optionsTravellers = [1, 2, , 3, 4, 5, 6, 7, 8, 9, 10];
+    const dateGo = ref(null);
+    const dateBack = ref(null);
+    const numberTravellers = ref(null);
+    const destination = ref(null);
+
+    const housings = ref({});
+
+    const storeHousingId = (id) => {
+      LocalStorage.set("housingId", id);
     };
-  }
 
+    const searchHousing = () => {
+            api
+        .post("/query",
+
+        {destination:destination.value}
+
+        )
+        .then((response) => {
+          housings.value = response.data;
+          console.log(response.data);
+
+          // stocker le reponse data et le fournir à la page de recherche
+        });
+    }
+
+    onMounted(async () => {
+      const response = await api.get("/query");
+      housings.value = response.data;
+      console.log(housings.value)
+    });
+
+    console.log(housings.value);
+    return {
+      housings,
+      storeHousingId,
+      searchHousing,
+      multipleDevoirs: ref(null),
+      multipleLimitations: ref(null),
+      dateGo,
+      dateBack,
+      numberTravellers,
+      destination,
+      optionsDevoirs,
+      optionsLimitations,
+      optionsTravellers,
+    };
+  },
 };
-
-  </script>
+</script>
